0a79f3fe7f8e59d2ac7d2e1ad47990fad2151ba3
mayoor-nk <88716230+mayoor-nk@users.noreply.github.com>
2023-11-20 21:15:40 +0530

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.example
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.example.ui.theme.ExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myButton: Button = findViewById(R.id.myButton)
+
+        myButton.setOnClickListener {
+
+            dispatch()
+        }
+
+
+
+    }
+    private fun dispatch(){
+        val REQUEST_IMAGE_CAPTURE=1
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ExampleTheme {
+        Greeting("Android")
+    }
+}
